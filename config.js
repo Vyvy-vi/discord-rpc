90d4dd3bfb3a748ff1073edd1725511c342a4de4
Vyvy-vi <vyom.j@protonmail.com>
2021-01-23 13:16:44 +0530

    docs: documented config file

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  // The client id can be obtained from the application on discord developer portal
   CLIENT_ID: "801410700689932298",
+  // Large image name is the name of the image assigned when it was uploaded in Rich Presence section on discord developer portal
   LARGE_IMG_NAME: "polygons",
+  // Floating text refers to the text, that appears when one hovers over the image in the rpc
   FLOATING_TEXT: "Vyvy-vi",
+  // Status is the description shown under the game-name:
   STATUS: ">:D",
+  // BUTTONS are clickable hyperlink buttons in your discord profile. The label attribute is the text displayed on the button, and the url is the url it wuld link to. NOTE- There is provision for only 2 buttons by discord.
   BUTTONS: [
     {label: "Github", url: "https://github.com/Vyvy-vi"},
     {label: "Support", url: "http://chat.vyvy-vi.ninja"}
